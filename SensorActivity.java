902891345a9695a263d51d27a406b0b94e81620d
aravindu-code <53659557+aravindu-code@users.noreply.github.com>
2019-08-28 21:53:17 +0530

    Add files via upload

---
+++ SensorActivity.java
@@ -0,0 +1,41 @@
+public class SensorActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor mLight;
+
+    @Override
+    public final void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event) {
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        float lux = event.values[0];
+        //Log.d(TAG, msg "onSensorChanged: lux");
+        // Do something with this sensor value.
+        Log.v(TAG, "onSensorChanged: lux");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
